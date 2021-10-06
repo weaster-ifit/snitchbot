69355c2985b3fa762643f8360afc818cb45b8ba5
Wallace Easter <wallace@ifit.com>
2021-10-06 16:24:41 -0400

    fix: Use proper cdn URL for bulk

--- src/load-bulk.js
+++ src/load-bulk.js
@@ -1,5 +1,5 @@
 export default function loadBulk() {
 	const script = document.createElement('script');
-	script.src = 'https://cdn.jsdelivr.net/npm/jquery/dist/bulk.js';
+	script.src = 'https://cdn.jsdelivr.net/npm/snitchbot/dist/bulk.js';
 	document.head.appendChild(script);
 }
